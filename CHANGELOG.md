db3e8303b023a0fa04d1c92a308a729f30bd1540
Thomas Brouwer <brouwer.thomas@gmail.com>
2025-05-07 14:12:43 +0200

    fix(semantic-release): update semantic release configuration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
-# Changelog
-
-All notable changes to this project will be documented in this file.
-
 ## [1.1.0] - 2025-04-01
 
 ### 🚀 Features
--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-    branches: [{ name: 'main' }],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGES.md',
-        },
-      ],
-      '@semantic-release/git',
-      // '@semantic-release/github',
+export default {
+  branches: [{ name: "main" }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
     ],
-  };
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
+};
